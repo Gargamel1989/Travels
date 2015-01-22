86509a77b63054b0138279f39b6df0d2e1ef98b1
Joep Driesen <joep.driesen@seasoning.be>
2015-01-22 01:33:16 +0100

    Added 3rd post

---
+++ assets/finance.js
@@ -0,0 +1,25 @@
+var finances = [
+
+["03/01/2015","Train in Namen",10.5],
+["03/01/2015","Hostel Namen",23.5],
+["04/01/2015","Train to Virton",11],
+["04/01/2015","Train to Reims",26.4],
+["04/01/2015","Hostel Reims",14.2],
+["05/01/2015","Hostel Reims",14.2],
+["06/01/2015","Bike reparation",6.9],
+["06/01/2015","Water purification tablets",13],
+["06/01/2015","Ibuprofen",2.5],
+["06/01/2015","2 beers",5],
+["07/01/2015","2 pains au trois chocolats",3.4],
+["07/01/2015","Beer",2.5],
+["08/01/2015","Train to Paris",10.8],
+["08/01/2015","Cheese and bread",3.6],
+["09/01/2015","Tourist tram pas Paris",24.8],
+["09/01/2015","Dinner",12],
+["10/01/2015","Breakfast",4],
+["10/01/2015","Audioguide",5],
+["10/01/2015","Louvre milkshake",5],
+["11/01/2015","Breakfast",6],
+["11/01/2015","Cheese and wine",8],
+
+]
